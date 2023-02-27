80596542d565f819019ad30bd03a9670d56cb76d
cmontague4 <90429535+cmontague4@users.noreply.github.com>
2023-02-27 13:37:12 -0500

    Update gradebook.h

--- Final Code/gradebook.h
+++ Final Code/gradebook.h
@@ -13,7 +13,7 @@ class Gradebook {
 
     public:
         Gradebook(std::vector<std::string> &names, std::vector<std::vector<int> > &labs, std::vector<std::vector<int> > &assignments, std::vector<std::vector<int> > &projects, std::vector<std::vector<int> > exams);
-        void get_student(); //requests name of student for inquiry
+        int get_student(); //requests name of student for inquiry
         void display_students(); //displays names of all students
         void category_grades(); //calculates and displays grades in one category
         void change_grade(); //requests a grade to be changed in the vector
