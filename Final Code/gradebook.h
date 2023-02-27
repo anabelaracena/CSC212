8edf893cd4391b1fc462f0c4668934fd78007282
cmontague4 <90429535+cmontague4@users.noreply.github.com>
2023-02-27 11:32:58 -0500

    Update gradebook.h

--- Final Code/gradebook.h
+++ Final Code/gradebook.h
@@ -1,25 +1,27 @@
+#pragma once
 #include <vector>
 #include <string>
 
 class Gradebook {
-    Private:
+    
+    private:
         std::vector<std::string> students;
         std::vector<std::vector<int> > labs;
         std::vector<std::vector<int> > assignments;
         std::vector<std::vector<int> > projects;
         std::vector<std::vector<int> > exams;
 
-    Public:
-        Gradebook::Gradebook(std::vector<std::string> &names, std::vector<std::vector<int> > &labs, std::vector<std::vector<int> > &assignments, std::vector<std::vector<int> > &projects, std::vector<std::vector<int> > exams);
-        Gradebook::get_student();
-        Gradebook::display_students()
-        Gradebook::category_grades();
-        Gradebook::change_grade();
-        Gradebook::final_grade();
-        Gradebook::all_grades();
-        Gradebook::get_categories();
-        Gradebook::get_overall();
-        Gradebook::is_exempt();
-        Gradebook::update_gradebook(std::string file_name);
+    public:
+        Gradebook(std::vector<std::string> &names, std::vector<std::vector<int> > &labs, std::vector<std::vector<int> > &assignments, std::vector<std::vector<int> > &projects, std::vector<std::vector<int> > exams);
+        void get_student();
+        void display_students();
+        void category_grades();
+        void change_grade();
+        void final_grade();
+        void all_grades();
+        void get_categories();
+        void get_overall();
+        void is_exempt();
+        void update_gradebook(std::string file_name);
 
 };
