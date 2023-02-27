187c77a9059da3b2dd55d1d35e2166c7c1aca5a3
frankdmurphy <123509830+frankdmurphy@users.noreply.github.com>
2023-02-27 12:47:46 -0500

    Update gradebook.h

--- Final Code/gradebook.h
+++ Final Code/gradebook.h
@@ -5,23 +5,23 @@
 class Gradebook {
     
     private:
-        std::vector<std::string> students;
-        std::vector<std::vector<int> > labs;
-        std::vector<std::vector<int> > assignments;
-        std::vector<std::vector<int> > projects;
-        std::vector<std::vector<int> > exams;
+        std::vector<std::string> students; //names of students
+        std::vector<std::vector<int> > labs; //values of labs
+        std::vector<std::vector<int> > assignments; //values of assignments
+        std::vector<std::vector<int> > projects; //values of projects
+        std::vector<std::vector<int> > exams; //values of exams
 
     public:
         Gradebook(std::vector<std::string> &names, std::vector<std::vector<int> > &labs, std::vector<std::vector<int> > &assignments, std::vector<std::vector<int> > &projects, std::vector<std::vector<int> > exams);
-        void get_student();
-        void display_students();
-        void category_grades();
-        void change_grade();
-        void final_grade();
-        void all_grades();
-        void get_categories();
-        void get_overall();
-        void is_exempt();
-        void update_gradebook(std::string file_name);
+        void get_student(); //requests name of student for inquiry
+        void display_students(); //displays names of all students
+        void category_grades(); //calculates and displays grades in one category
+        void change_grade(); //requests a grade to be changed in the vector
+        void final_grade(); //calculates and displays letter grade of student
+        void all_grades(); //calculates and displays every grade from every category + overall
+        void get_categories(); //calculates and displays total grade in categories + overall
+        void get_overall(); //calculates and displays the total overall grade
+        void is_exempt(); //tells user whether a student is exempt from exam
+        void update_gradebook(std::string file_name); //overwrites input file
 
 };
